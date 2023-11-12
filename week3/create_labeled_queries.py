cdf4a5af9ffa99dc08cfeabaeb148c1db9fc94af
Ilkcan Keles <cankelesilk@gmail.com>
2023-11-12 21:26:45 +0000

    week3 soln

--- week3/create_labeled_queries.py
+++ week3/create_labeled_queries.py
@@ -42,15 +42,62 @@ for child in root:
     if leaf_id != root_category_id:
         categories.append(leaf_id)
         parents.append(cat_path_ids[-2])
-parents_df = pd.DataFrame(list(zip(categories, parents)), columns =['category', 'parent'])
+
+# levels from categories 
+all_done = False
+levels = [-1] * len(parents)
+categories_copy = categories
+parents_copy = parents
+curr_level = 0
+max_level = -1
+while not all_done:
+    cur_leaf_categories = [value for value in categories_copy if value not in parents_copy]
+    leaf_indices = []
+    for leaf_cat in cur_leaf_categories:
+        actual_idx = categories.index(leaf_cat)
+        copy_leaf_idx = categories_copy.index(leaf_cat)
+        leaf_indices.append(copy_leaf_idx)
+        levels[actual_idx] = curr_level
+    categories_copy = [j for i, j in enumerate(categories_copy) if i not in leaf_indices]
+    parents_copy = [j for i, j in enumerate(parents_copy) if i not in leaf_indices]
+    if len(categories_copy) == 0:
+        all_done = True
+    else:
+        curr_level += 1
+
+max_level = curr_level
+
+parents_df = pd.DataFrame(list(zip(categories, parents, levels)), columns =['category', 'parent', 'level'])
+print(parents_df)
 
 # Read the training data into pandas, only keeping queries with non-root categories in our category tree.
 queries_df = pd.read_csv(queries_file_name)[['category', 'query']]
 queries_df = queries_df[queries_df['category'].isin(categories)]
 
 # IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+queries_df['query'] = queries_df['query'].str.lower()
+queries_df['tokens'] = queries_df['query'].str.split()
+queries_df['stemmed_tokens'] = queries_df['tokens'].apply(lambda x: [stemmer.stem(y) for y in x])
+queries_df['query'] = queries_df['stemmed_tokens'].str.join(' ')
 
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+queries_df_with_counts = queries_df.groupby('category').size().reset_index(name='count')
+queries_df_merged = queries_df.merge(queries_df_with_counts, how='left', on='category').merge(parents_df, how='left', on='category')
+print("Number of sub-threshold categories: " + str(len(queries_df_merged[queries_df_merged['count'] < min_queries])))
+curr_level = 0
+while len(queries_df_merged[queries_df_merged['count'] < min_queries]) > 0:
+    print(f"Curr level: {curr_level}")
+    queries_df_merged.loc[(queries_df_merged['count'] < min_queries) & (queries_df_merged['level'] == curr_level), 'category'] = queries_df_merged['parent']
+    # queries_df_merged.loc[queries_df_merged['count'] < min_queries, 'category'] = queries_df_merged['parent']
+    queries_df = queries_df_merged[['category', 'query']]
+    queries_df = queries_df[queries_df['category'].isin(categories)]
+    queries_df_with_counts = queries_df.groupby('category').size().reset_index(name='count')
+    queries_df_merged = queries_df.merge(queries_df_with_counts, how='left', on='category').merge(parents_df, how='left', on='category')
+    print(f"Number of unique categories: {queries_df_merged['category'].nunique()}")
+    print("Number of sub-threshold categories: " + str(len(queries_df_merged[queries_df_merged['count'] < min_queries])))
+    curr_level += 1
+
+print(f"Number of unique categories: {queries_df['category'].nunique()}")
 
 # Create labels in fastText format.
 queries_df['label'] = '__label__' + queries_df['category']
---
+++ week3/project_commands.sh
@@ -0,0 +1,13 @@
+python week3/create_labeled_queries.py
+wc /workspace/datasets/fasttext/labeled_queries.txt
+head /workspace/datasets/fasttext/labeled_queries.txt
+cut -d' ' -f1 /workspace/datasets/fasttext/labeled_queries.txt | sort | uniq | wc
+
+python week3/create_labeled_queries.py --min_queries 1000
+shuf /workspace/datasets/fasttext/labeled_queries.txt > /workspace/datasets/fasttext/shuffled_labeled_queries.txt
+
+head -50000 /workspace/datasets/fasttext/shuffled_labeled_queries.txt > /workspace/datasets/fasttext/queries_training_data.txt
+tail -10000 /workspace/datasets/fasttext/shuffled_labeled_queries.txt > /workspace/datasets/fasttext/queries_test_data.txt
+
+~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/fasttext/queries_training_data.txt -output query_classifier -lr 0.5 -epoch 25 -wordNgrams 2
+~/fastText-0.9.2/fasttext test query_classifier.bin /workspace/datasets/fasttext/queries_test_data.txt
